13cb8aeb64c3d4a9c7c9ed3e8749bbc419ad2d14
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2024-02-25 12:34:20 +0530

    passwords bcrypted

--- server/controller/userapi.js
+++ server/controller/userapi.js
@@ -1,7 +1,14 @@
 import User from "../model/userscheme.js";
+import bcrypt from "bcrypt";
 const Signup = async (request, response) => {
   try {
-    const user = request.body;
+    // const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(request.body.password, 10);
+    const user = {
+      username: request.body.username,
+      name: request.body.name,
+      password: hashedPassword,
+    };
 
     const newUser = new User(user); // creating new user
     await newUser.save(); // saving that user in the database through save method of database
